3184e273157785525bb18e94836959c87573860e
Hossein Hamid <HHamid@basemap.co.uk>
2023-10-20 16:03:18 +0100

    new starter page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+  <app-header />
   <img alt="Vue logo" src="./assets/logo.png">
+  
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue'
+
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
     HelloWorld
   }
 }
